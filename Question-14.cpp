420900e6ee217fda82e89051aa8f24de739a22d7
shashankkes <shashankkes.1997@gamail.com>
2020-05-26 22:11:08 +0530

    Bubble-sort

---
+++ Question-14.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+int swap(int *a , int *b){//it will swap the elements
+		int temp=*a;
+		*a=*b;
+		*b=temp;	
+}
+int union_intersection(int arr[],int n){
+	while(i!=n-1 || j!=m-1){
+		
+	}
+int main(){
+	cout<<"Enter the no. of values in the arry: ";
+	int n,m;
+	cin>>n>>m;
+	int a[n],b[m];
+	for(int i=0;i<n;i++)// input for the array 1
+	cin>>a[i];
+	for(int i=0;i<n;i++)// input for the array 2
+	cin>>b[i];
+	bubble(a,b,n,m);
+}
